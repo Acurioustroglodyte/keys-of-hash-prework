dee9fb1bb50c635dc32252cc84d0dd9029f2a544
Trevor Jameson <tjameson15@gmail.com>
2018-08-18 21:07:47 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,7 +2,8 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    return self.collect do |animal, country|
+
+    matched_keys = self.collect do |animal, country|
       if arguments.include?(country)
         animal
       end
