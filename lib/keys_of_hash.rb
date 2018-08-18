ddf283bfec3f4a5f312fcde9d62652aa2e63716a
Trevor Jameson <tjameson15@gmail.com>
2018-08-18 21:17:27 +0000

    automatically backed up by learn

--- lib/keys_of_hash.rb
+++ lib/keys_of_hash.rb
@@ -2,13 +2,10 @@ require 'pry'
 
 class Hash
   def keys_of(*arguments)
-    # You're on the right track.
     matched_keys = []
     self.each do |animal, country|
       if arguments.include?(country)
         matched_keys << animal
-      else
-        # Add nothing to matched_keys collection
       end
     end
     matched_keys
